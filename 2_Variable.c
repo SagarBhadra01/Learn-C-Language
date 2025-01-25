1eb6ce4331445b4c4ef787ba049d28d5c874c2fd
SagarBhadra01 <sagarbhadra404@gmail.com>
2025-01-25 22:31:54 +0530

    Update 2_Variable.c

--- 2_Variable.c
+++ 2_Variable.c
@@ -21,11 +21,24 @@ int main ()
     printf(" Print a= %d \n", a);
     printf(" Print b= %f \n", b);
     printf(" Print c= %c \n", c);
-    // Format Specifiers for int- %d 
+    // Format Specifiers for int- %d or %i
     // Format Specifiers for float- %f 
     // Format Specifiers for char- %c
 
+    //we can write multiple variable value together, like-
+    int d,e,f;
 
+    //double data type
+    double g=10.12345678987654321;
+    float h=10.12345678987654321;
+    printf(" Print g= %lf \n", g);
+    printf(" Print h= %f \n", h);
 
+    
+    //size-
+    // int	: 	2 or 4 bytes 
+    // float:	4 bytes
+    // double: 	8 bytes	
+    // char :	1 bytes	
 
 }
