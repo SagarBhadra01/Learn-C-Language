cc9e285923e99b8969f2caac0b4c44b752424d91
SagarBhadra01 <sagarbhadra404@gmail.com>
2025-01-25 16:30:52 +0530

    Create 2_Variable.c

---
+++ 2_Variable.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main ()
+{
+    //declaring variables
+    int a;
+    float b;
+    char c;
+    //here a,b,c are variable name
+    // A variable name can only contain alphabets, digits, and underscores(_).
+    // A variable cannot start with a digit.
+    // A variable cannot include any white space in its name.
+    // The name should not be a reserved keyword or any special character.
+
+
+    //storing data
+    a= 10;
+    b= 6.9;
+    c= 'S'; // use-> ''
+ 
+    //print all data
+    printf(" Print a= %d \n", a);
+    printf(" Print b= %f \n", b);
+    printf(" Print c= %c \n", c);
+    // Format Specifiers for int- %d 
+    // Format Specifiers for float- %f 
+    // Format Specifiers for char- %c
+
+
+
+
+}
