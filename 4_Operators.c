666a4437b1e1aa59840aade4af58fbfd712e7503
SagarBhadra01 <sagarbhadra404@gmail.com>
2025-01-27 01:15:25 +0530

    Update 4_Operators.c

--- 4_Operators.c
+++ 4_Operators.c
@@ -11,8 +11,8 @@ int main(){
     printf("5. a % b = %d\n", a % b);  //(Modulus) Returns the remainder after diving the left operand with the right operand.	
     printf("6. +a = %d\n", +a);        //(Unary Plus) Used to specify the positive values.	
     printf("7. -a = %d\n", -a);        //(Unary Minus) Flips the sign of the value.	
-    printf("8. a++ = %d\n", a++);      //(Increment) Increases the value of the operand by 1.	
-    printf("9. a-- = %d\n", a--);      //(Decrement) Decreases the value of the operand by 1.	
+    printf("8. ++a = %d\n", ++a);      //(Increment) Increases the value of the operand by 1.	
+    printf("9. --b = %d\n", --b);      //(Decrement) Decreases the value of the operand by 1.	
 
 
     //Relational Operators ->
@@ -22,14 +22,18 @@ int main(){
     printf("13. a >= b: %d\n", a >= b);  //(Greater than Equal) Returns true if the left operand is greater than or equal to right operand. Else false
     printf("14. a == b: %d\n", a == b);  //(Equal) Returns true if both the operands are equal.	
     printf("15. a != b : %d\n", a != b); //(Not Equal) Returns true if both the operands are NOT equal.
-    //here true=1, false=0clear
+    //here true=1, false=0
 
 
 
     //Logical Operators ->
     printf("16. a && b : %d\n", a && b);  //(Logical AND Gate) Returns true if both the operands are true.	
     printf("17. a || b : %d\n", a || b);  //(Logical OR Gate) Returns true if both or any of the operand is true.
-    printf("18. !a: %d\n", !a);           //(Logical NOT Gate) Returns true if the operand is false.	
+    printf("18. !a: %d\n", !a);           //(Logical NOT Gate) Returns true if the operand is false.
+
+
+    // Assighment operators ->
+
 
 
     return 0;
