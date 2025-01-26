25c107fe4944f4116ead3bf9e69931a45b5f6456
SagarBhadra01 <sagarbhadra404@gmail.com>
2025-01-26 23:28:34 +0530

    Update 1_Structure.c

--- 1_Structure.c
+++ 1_Structure.c
@@ -3,4 +3,10 @@ int main()                      //main
 {
   printf("Hello World!");       //statement
   return 0;                     //return
-}
+}                               //{} -> body
+
+
+//C is a general- purpose  programming language created by Dennis Ritchie at Bell Laboratories in 1972.
+//Unix operating system,MySQL database, most part of Ruby,Pearl,apache,etc was written by C-Language.
+//Many later languages have borrowed syntex/features directly or indirectly from c, like the syntex of JAVA,PHP,JS and many more that are mainly based on C.
+//C++ is a super set of C.
