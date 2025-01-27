80f411a0b2b9fca68b93a8ea116ce200e38f0261
SagarBhadra01 <sagarbhadra404@gmail.com>
2025-01-27 22:57:08 +0530

    Create 5_Keywords.c

---
+++ 5_Keywords.c
@@ -0,0 +1,34 @@
+// There are 32 Keywords ->
+
+// auto:
+// break:	Breaks out of a loop or a switch block
+// case:	Marks a block of code in switch statements
+// char:	A data type that can store a single character
+// const:	Defines a variable or parameter as a constant (unchangeable)
+// continue:	Continues to the next iteration of a loop
+// default:	Specifies the default block of code in a switch statement
+// do:	Used together with while to create a do/while loop
+// double:	A data type that is usually 64 bits long which can store fractional numbers
+// else:	Used in conditional statements
+// enum:	Declares an enumerated type
+// extern:
+// float:	A data type that is usually 32 bits long which can store fractional numbers
+// for:	Creates a for loop
+// goto:	Jumps to a line of code specified by a label
+// if:	Makes a conditional statement
+// int:	A data type that is usually 32 bits long which can store whole numbers
+// long:	Ensures that an integer is at least 32 bits long (use long long to ensure 64 bits)
+// return:	Used to return a value from a function
+// register:
+// short:	Reduces the size of an integer to 16 bits
+// signed:	Specifies that an int or char can represent both positive and negative values (for int this is the default so the keyword is not usually necessary)
+// sizeof:	An operator that returns the amount of memory occupied by a variable or data type
+// static:	Specifies that a variable in a function keeps its value after the function ends
+// struct:	Defines a structure
+// switch:	Selects one of many code blocks to be executed
+// typedef:	Defines a custom data type
+// union:
+// unsigned:	Specifies that an int or char should only represent positive values which allows for storing numbers up to twice as large
+// void:	Indicates a function that does not return a value or specifies a pointer to a data with an unspecified type
+// volatile:
+// while:	Creates a while loop
