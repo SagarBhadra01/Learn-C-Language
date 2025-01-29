e7022e355e07978d645e08f202ca4138bc0c3911
SagarBhadra01 <sagarbhadra404@gmail.com>
2025-01-29 22:39:09 +0530

    Create 7_if_condition.c

---
+++ 7_if_condition.c
@@ -0,0 +1,88 @@
+//Struture-
+
+//1. if condition
+/*if (condition)
+{
+    true 
+}*/
+
+//2. if-else condition
+/*if (condition)
+{
+    //true 
+}
+else
+{
+    //false 
+}*/
+
+//3. if-else if condition
+/*if (condition)
+{
+    //true 
+}
+else if (condition)
+{
+    //true 
+}
+else
+{
+    //false 
+}*/
+
+//4. nested if-else condition
+/*if (condition)
+{
+    if (condition)
+    {
+        //true 
+    }
+    else
+    {
+        //false 
+    }
+}
+else
+{
+    if (condition)
+    {
+        //true 
+    }
+    else
+    {
+        //false 
+    }
+}*/
+
+
+#include <stdio.h>
+
+int main(){
+    //if condition->
+    int n =9;
+    if (n < 10) {
+        printf("%d < 10 \n", n);
+    }
+
+    //if else condition->
+    int m;
+    printf("enter the value of m:",m);
+    scanf("%d", &m);
+    if (m < 10) {
+        printf("%d < 10", m);
+    }
+    else
+    {
+        printf("%d > 10", m);
+    }
+
+    //if-elseif condition->
+    
+    
+
+
+
+
+
+    return 0;
+}
