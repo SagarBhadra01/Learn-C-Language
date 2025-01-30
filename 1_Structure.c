7adba284199a090caee491409b2975ed2b2cd65d
Sagar Bhadra <sagarbhadra404@gmail.com>
2025-01-30 12:38:50 +0530

    Update 1_Structure.c

--- 1_Structure.c
+++ 1_Structure.c
@@ -25,20 +25,3 @@ int main()                      //main
 // Period (.): Used to access members of a structure or union.
 // Tilde(~): Bitwise One’s Complement Operator.
 
-#include <stdio.h>
-
-int main() {
-    int n = 9;
-  
-    // if statement with true condition
-    if (n < 10) {
-        printf("%d < 10", n);
-    }
-
-    // if statement with false condition
-    if (n > 20) {
-        printf("%d > 20", n);
-    }
-
-    return 0;
-}
