af18749fff4c9015ec065bf940b5238534944125
SagarBhadra01 <sagarbhadra404@gmail.com>
2025-02-06 23:04:34 +0530

    Create 11_String.c

---
+++ 11_String.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    //assigning string without size
+    char str1[] = "SagarBhadra";
+
+    //assigning string with size
+    char str2[20] = "SagarBhadra";
+
+    printf("%s",str2);
+
+
+
+
+
+    return 0;
+}
