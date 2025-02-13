4c3cb57baa8f18efd396898a42078dd18988339a
SagarBhadra01 <sagarbhadra404@gmail.com>
2025-02-13 20:20:52 +0530

    Update 11_String.c

--- 11_String.c
+++ 11_String.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
     //assigning string without size
@@ -6,12 +7,25 @@ int main(){
 
     //assigning string with size
     char str2[20] = "SagarBhadra";
-
     printf("%s",str2);
 
+    //assigning by user
+    char str3[10];
+    printf("\n put string:");
+    scanf("%s", str3);
+    printf("%s\n", str3);
 
-
-
-
+    // Creating array of strings for 3 strings with max length of each string as 10
+    char arr[3][10] = {"Sagar", "Sagar", "Sagar"};
+    for (int i = 0; i < 3; i++){
+       printf("%s\n", arr[i]);
+    }
+    /*Syntax-
+    char arr_name[r][m] = {s1, s2, …., sn};
+    Here,
+    arr_name: Name of the variable.
+    r: Maximum number of strings to be stored in the array
+    m: Maximum number of character values that can be stored in each string.
+    s1, s2, …., sn: Strings to be stored.*/
     return 0;
 }
